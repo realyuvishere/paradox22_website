66ce9bcdd40062a623e2c3f743ca7d7dd23496a1
Manish <new14mail993@gmail.com>
2022-05-16 22:51:09 +0530

    our-team

--- members.js
+++ members.js
@@ -1,8 +1,8 @@
 const festteam = [
     {
         name: 'Gokulakrishnan M',
-        tagline: '',
-        image: 'https://i.imgur.com/0KLK0KO.jpg',
+        tagline: 'Man of action',
+        image: 'https://i.imgur.com/27NEgUP.jpg',
         studentId: '21f1007026',
         social: {
             linkedin: 'https://www.linkedin.com/in/gurneet-bhuller',
@@ -13,8 +13,8 @@ const festteam = [
     },
     {
         name: 'Gurneet Kaur Bhuller',
-        tagline: '',
-        image: 'https://i.imgur.com/XIv1pWq.jpg',
+        tagline: 'I\'m somebody who is strong willed and isn\'t afraid to go after things I\'m passionate about',
+        image: 'https://i.imgur.com/qnmN8r1.jpg',
         studentId: '21f2000672',
         social: {
             linkedin: 'https://www.linkedin.com/in/gurneet-bhuller',
@@ -38,7 +38,7 @@ const festteam = [
     {
         name: 'Vibhore Agarwal',
         tagline: '',
-        image: 'https://i.imgur.com/ayHAgAJ.jpg',
+        image: 'https://i.imgur.com/XMjmMic.jpg',
         studentId: '21f1004126',
         social: {
             linkedin: 'https://www.linkedin.com/in/vibhore-agrawal',
@@ -50,7 +50,122 @@ const festteam = [
 ]
 
 const webteam = [
-
+    {
+        name: 'Yuvraj Sharma',
+        tagline: 'Can play music, Can write code, Can click pictures, Can make food and videos but Can\'t dance',
+        image: 'https://i.imgur.com/GgzPw9s.jpg',
+        studentId: '21f3000180',
+        social: {
+            linkedin: 'https://www.linkedin.com/in/yuvishere',
+            instagram: 'https://www.instagram.com/yuv_is_here/',
+            twitter: 'https://www.twitter.com/realyuvishere',
+            facebook: '',
+            github: 'https://github.com/realyuvishere'
+        },
+    },
+    {
+        name: 'S Manish',
+        tagline: 'Just a bLanK homosapien',
+        image: 'https://i.imgur.com/ACjINOF.jpg',
+        studentId: '21f3002911',
+        social: {
+            linkedin: '',
+            instagram: '',
+            twitter: '',
+            facebook: '',
+            github:''
+        },
+    },
+    {
+        name: 'Ashwin Hebbar',
+        tagline: 'No point really, it\'s a circle',
+        image: 'https://i.imgur.com/1lBkELv.jpg',
+        studentId: '21f1003155',
+        social: {
+            linkedin: 'https://www.linkedin.com/in/ashwin-hebbar/',
+            instagram: '',
+            twitter: '',
+            facebook: '',
+            github:'https://github.com/AshwinHebbar314/'
+        },
+    },
+    {
+        name: 'Abul Aman',
+        tagline: 'I get my hands dirty on the Keyboard all day everyday, powered by Raw Curiosity and some Strong Coffee...',
+        image: 'https://i.imgur.com/T51o8US.jpg',
+        studentId: '21f1000506',
+        social: {
+            linkedin: 'https://www.linkedin.com/in/abul-aman-67481a222',
+            instagram: '',
+            twitter: '',
+            facebook: '',
+            github:'https://github.com/abulaman8'
+        },
+    },
+    {
+        name: 'Harshanand Sharma',
+        tagline: 'Sometimes Devil is better than Evil',
+        image: 'https://i.imgur.com/Zq2pWWo.jpg',
+        studentId: '21f3000881',
+        social: {
+            linkedin: 'https://www.linkedin.com/in/harshanand-sharma-13354b151/',
+            instagram: '',
+            twitter: '',
+            facebook: ''
+        },
+    },
+    {
+        name: 'Hetav Chauhan',
+        tagline: 'Process more important than result.',
+        image: 'https://i.imgur.com/rhC0zQK.jpg',
+        studentId: '22f1000101',
+        social: {
+            linkedin: 'https://www.linkedin.com/in/hetav-chauhan',
+            instagram: '',
+            twitter: '',
+            facebook: '',
+            github:'https://github.com/hetavchauhan'
+        },
+    },
+    {
+        name: 'Ayush Kumar',
+        tagline: 'I\'m a web developer, programmer, foody & likes travelling. I can\'t change the direction of wind, but adjust my sails to always reach my destination.',
+        image: 'https://i.imgur.com/WPTXo2h.jpg',
+        studentId: '21f3002911',
+        social: {
+            linkedin: 'https://www.linkedin.com/in/ayush838/',
+            instagram: 'https://www.instagram.com/k_ayush_38/',
+            twitter: '',
+            facebook: '',
+            github:'https://github.com/aayush138'
+        },
+    },
+    {
+        name: 'Aryan Mishra',
+        tagline: 'Process more important than result.',
+        image: 'https://i.imgur.com/4oXfZCL.jpg',
+        studentId: '21f3002911',
+        social: {
+            linkedin: '',
+            instagram: '',
+            twitter: '',
+            facebook: '',
+            github:''
+        },
+    },
+    {
+        name: 'Nikhil Isaac',
+        tagline: 'Process more important than result.',
+        image: 'https://i.imgur.com/u3Cwx4V.jpg',
+        studentId: '21f1000048',
+        social: {
+            linkedin: 'https://www.linkedin.com/in/nikhil-isaac/',
+            instagram: '',
+            twitter: '',
+            facebook: '',
+            github:'https://github.com/nikhilisaac'
+        },
+    }
 ]
 
 module.exports = {

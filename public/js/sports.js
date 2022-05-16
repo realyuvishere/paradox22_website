8f42d36bc81296625532a062537c0dbd119e32e4
5.413U14M4N <abulaman6@gmail.com>
2022-05-16 09:29:35 +0530

    minor changes

--- public/js/sports.js
+++ public/js/sports.js
@@ -1,3 +1,11 @@
+window.addEventListener("load", function() {
+    const first = document.querySelectorAll('[data-event-tab-content]')[0]
+    first.classList.add('active')
+});
+
+
+
+
 const tabs = document.querySelectorAll('[data-event-tab-target]')
 const tabContents = document.querySelectorAll('[data-event-tab-content]')
 
@@ -6,10 +14,10 @@ tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(`#${tab.dataset.eventTabTarget}`)
         tabContents.forEach(tabContent => tabContent.classList.remove('active'))
-        // console.log('removed active')
+            // console.log('removed active')
         tabs.forEach(tab => tab.classList.remove('active'))
         tab.classList.add('active')
         target.classList.add('active')
-        // console.log('added active')
+            // console.log('added active')
     })
 })

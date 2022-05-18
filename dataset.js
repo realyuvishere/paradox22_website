ab918178ae130ccc34cc4ad715bda306f23c119e
Manish <new14mail993@gmail.com>
2022-05-18 13:56:42 +0530

    football 5s

--- dataset.js
+++ dataset.js
@@ -1,17 +1,14 @@
-const sports = [
-    {
+const sports = [{
         title: 'Badminton',
         url: '',
         imageID: 'badminton.png',
         desc: "Tired? Refresh with <b>Badminton</b>. It is a fast-pace, fun workout game that offers big rewards to players of all ages. We plan to host Doubles and mixed doubles based on the preference of the participants. Each game will be of 21 pointers of single set.",
         date: "20th May, 06:00am-10:00am",
         venue: "20/05/2022 - SAC",
-        contact: [
-            {
-                name: "Rahul jaiswal",
-                email: "21f1003040@student.onlinedegree.iitm.ac.in",
-            }
-        ]
+        contact: [{
+            name: "Rahul jaiswal",
+            email: "21f1003040@student.onlinedegree.iitm.ac.in",
+        }]
     },
     {
         title: 'Basketball',
@@ -20,12 +17,10 @@ const sports = [
         desc: "<b>Basketball</b>, a fast game which wakes you up from the sleep of annoying thoughts and brings your mind to the present moment so as to try every possibility of winning the game. We, the team of PARADOX'22, brought this game with a little twist and lots of fun. All of us have played Basketball in teams. Now, it's time to play it as an individual. You can showcase your skills and cutting moves. The matches will be played 1v1 in knockout format. Each game will be a 5-pointer match. We also have a surprise for you. So what are you waiting for? Go and register for it... Meet you at the court.",
         date: "20th May, 07:00am-09:00am",
         venue: "20/05/2022 - Sports complex Basketball court",
-        contact: [
-            {
-                name: "Uday Agrawal",
-                email: "21f3001708@student.onlinedegree.iitm.ac.in",
-            }
-        ]
+        contact: [{
+            name: "Uday Agrawal",
+            email: "21f3001708@student.onlinedegree.iitm.ac.in",
+        }]
     },
     {
         title: "Box Cricket",
@@ -34,8 +29,7 @@ const sports = [
         desc: "<b>Cricket</b> is an insanely popular game in India. But it is almost always impossible for us cricket enthusiasts to play the game of cricket the way we watch it on TV. This is where the concept of box cricket comes into play. It is mostly 5-10 overs per innings match with some interesting and fun rules. Cricket itself is a religion in India and adding a flavor in the game will increase the value in the entire event.",
         date: "20th May, 06:00am-10:00am",
         venue: "20/05/2022 - Chemplast Cricket Ground",
-        contact: [
-            {
+        contact: [{
                 name: "Rajkishore Nandi",
                 email: "21f1006016@student.onlinedegree.iitm.ac.in",
             },
@@ -52,12 +46,10 @@ const sports = [
         desc: "Inclusive 5Km Run taking you through the most scenic routes of _**IIT-Madras Campus**_**.** You will witness Mesmerizing Sun-Rise, Wildlife & Nature at its best. Run, Jog or walk, you will cross the finish line Smiling :).<br><br> Bonus: Bib Trackable photographs.",
         date: "21st May, 05:30am-07:30am",
         venue: "21/05/2022 - IITM Campus Kendriya Vidyalaya",
-        contact: [
-            {
-                name: "Krishna",
-                email: "21f3001825@student.onlinedegree.iitm.ac.in",
-            }
-        ]
+        contact: [{
+            name: "Krishna",
+            email: "21f3001825@student.onlinedegree.iitm.ac.in",
+        }]
     },
     {
         title: "Table Tennis",
@@ -66,8 +58,7 @@ const sports = [
         desc: "Sport means accuracy & perfection and you will find the perfect example of both of them in the game of Table Tennis. A Doubles-only knockout event has been planned for TT. Each game will be a 11-pointer and each match will be a best-of-3 game affair.",
         date: "20th May, 08:00am-10:00am",
         venue: "20/05/2022 - Sports complex ground floor Table Tennis",
-        contact: [
-            {
+        contact: [{
                 name: "Somsankar Chakraborty",
                 email: "21f3003023@student.onlinedegree.iitm.ac.in",
             },
@@ -84,8 +75,7 @@ const sports = [
         desc: "Treasure Hunt is a team event. Each team will have a different story and clues according to their story. Clues will be in the form of puzzles. Teams will have to solve the puzzle leading to the next clue. Hunt will have 4 stages and in each stage, they will get a new clue. The team completes the hunt when they solve the final puzzle. The first 3 teams to complete the hunt will be given some prizes/goodies.",
         date: "20th May, 7:00 AM - 10:00 AM",
         venue: "20/05/2022 - Outside OAT",
-        contact: [
-            {
+        contact: [{
                 name: "Sankalp Shrivastava",
                 email: "21f1006134@student.onlinedegree.iitm.ac.in",
             },
@@ -102,8 +92,7 @@ const sports = [
         desc: "Let's begin the mind game known as Shah-Maat Chess Tournament!! <br><br> After the online qualifier 32 people will be selected. In the campus among those 32 people there 16 will be selected through the first knockout. Then from there 8 will be selected through the second knockout. Those 8 will play a 5+0 blitz tournament.",
         date: "20th May, 08.00 am - 10.00 am",
         venue: "20/05/2022 - Sports complex 2nd floor Chess room",
-        contact: [
-            {
+        contact: [{
                 name: "Manidipa Roy",
                 email: "21f1006134@student.onlinedegree.iitm.ac.in",
             },
@@ -124,29 +113,41 @@ const sports = [
         desc: "An exciting Cricket match between the POD Members(Faculty/Instructors) and the Fest Organizers (Students). This will be an exciting match!",
         date: "21st May, 06:00am-8:30am",
         venue: "21/05/2022 - Kendriya Vidyalaya",
-        contact: [
-            {
-                name: "Updated Soon",
-                email: "Updated Soon",
-            }
-        ]
+        contact: [{
+            name: "Updated Soon",
+            email: "Updated Soon",
+        }]
+    },
+    {
+        title: "Football 5s",
+        url: '',
+        imageID: '.png',
+        desc: "Rules: <br> 1. Only 5 + 3 Players in each team, if found to have more then the team will be disqualified.<br>2. No offsides.<br>3. Match duration - prior to semi-final : 7-1-7 Mins | Semi-Finals : 10-5-10 Mins | Finals : 15-10-15<br>4. Subs on the fly(rolling substitution).<br> 5. No goalkeeper punts, the ball must stay below head height, otherwise possession and a free kick will be awarded to the opposing team.<br>6. No slide tackles, players must not go to ground to steal possession back.<br>7. The Referee's decision is final.<br><br>8. If match ends in tie:<br> &nbsp;&nbsp;&nbsp; a. 3 penalties each team (from playing 5); if still tied:<br> &nbsp;&nbsp;&nbsp; b. Open goal from half line, sudden death. (From playing 5)<br> &nbsp;&nbsp;&nbsp; c. Iff open goal crosses more than 8 kicks, Toss will decide the winners.<br> <br>9. Penalty rules:<br>&nbsp;&nbsp;&nbsp; a. One step kick. One leg is placed near/ next to the ball and the other one is swung to takethe kick.<br>&nbsp;&nbsp;&nbsp; b. No fake shots or misguidences through actions.<br>&nbsp;&nbsp;&nbsp; c. Free kicks and corners the opposing team has to be minimum of 5 yards away from theball.<br><br>10. Goalkeeper rules:<br>a. PENALTY RULES:<br>&nbsp;&nbsp;&nbsp; i. Should stand on the goalpost line.<br>&nbsp;&nbsp;&nbsp; ii. Cannot cross the line before the shooter kicks the ball<br><br>11. Side outs -<br>&nbsp;&nbsp;&nbsp; a. No Throw-ins from the sidelines. Instead the ball is placed on ground and passed in to ateammate with the foot.<br>&nbsp;&nbsp;&nbsp; b. Pass should not be played above hip level.<br>&nbsp;&nbsp;&nbsp; c. No direct goal from side",
+        date: "20th May, Time: 6:00 am to 10 :00 am",
+        venue: "Venue: Near Chemplast Ground, IITM",
+        contact: [{
+            name: "Chhayank Thakur",
+            email: "21f3002924@student.onlinedegree.iitm.ac.in",
+        },
+        {
+            name: "Krishna Dubey",
+            email: "21f2000813@student.onlinedegree.iitm.ac.in",
+        }
+    ]
     }
-    
+
 ]
 
-const cultural = [
-    {
+const cultural = [{
         title: "Icebreaker and Games",
         url: '',
         imageID: 'ice-breaker.jpg',
         desc: "A small informal session intended to help students is themselves into the environment of the campus. This session is a new opportunity for to meet and make new friends.",
         date: "20th May, 09:30 AM - 11:30 PM",
         venue: "Student Activity Center (SAC)",
-        contact: [
-            {
-                name:'Will be updated soon'
-            }
-        ]
+        contact: [{
+            name: 'Will be updated soon'
+        }]
     },
     {
         title: "Beyond the Words",
@@ -155,8 +156,7 @@ const cultural = [
         desc: "Beyond The Words is an Open Mic Event in the IITM Paradox'22. the Annual Fest of the IITM BSc Degree community. To share your feelings and emotions of experiences, pain, blessings, and happy moments in a form of words, like pure artwork, which melts the hearts of people as well as makes them smile, and remember this evening forever. We are inviting performers to represent their artwork to people and dive deep into their memories and heart. We would love to take your registration for our beautiful event, where you will perform in front of the epic gathering of a massive audience and live the moment onstage collecting a few lifelong memories.",
         date: "20th May, 03:00pm-05:00pm",
         venue: "Preliminary Round - 20/05/2022 <br> FINALE - 21/05/2022 - Student Activity Center (SAC)",
-        contact: [
-            {
+        contact: [{
                 name: "Shubham Kumar",
                 email: "21f3000285@student.onlinedegree.ittm.ac.in",
             },
@@ -173,12 +173,10 @@ const cultural = [
         desc: "Controversia is an event which provides you with an opportunity to sharpen your debating skills. \"The aim of argument, or of discussion, should not be victory, but progress.\" - Joseph Joubert.",
         date: "Round 1 - 04th May, 06:00pm-09:00pm - Online Qualifier Round <br> Round 2 - 20th May, 01:30pm-03:30pm - Preliminary Round <br> Round 3 - 21st May, 02:00pm-03:00pm - FINALE",
         venue: "Preliminary Round - 20/05/2022 <br> FINALE - 21/05/2022 - Student Activity Center (SAC)",
-        contact: [
-            {
-                name: "Sampriti Raha",
-                email: " 22f1000064@student.onlinedegree.iitm.ac.in",
-            }
-        ]
+        contact: [{
+            name: "Sampriti Raha",
+            email: " 22f1000064@student.onlinedegree.iitm.ac.in",
+        }]
     },
     {
         title: "Level Up",
@@ -187,12 +185,10 @@ const cultural = [
         desc: "Hola! Have you ever experienced the fun of letting your body flow naturally to the beats ? If not this is the chance.<br> IITM PARADOX '22 presents, LEVEL UP! <br> A pan-Indian dance event to unleash your inner MJ! LEVEL UP the bars with your moves.",
         date: "20th May, 06:00pm-07:30pm",
         venue: "20/05/2022 - Student Activity Center (SAC)",
-        contact: [
-            {
-                name: "Tanya Jha",
-                email: "21f2001453@student.onlinedegree.iitm.ac.in",
-            }
-        ]
+        contact: [{
+            name: "Tanya Jha",
+            email: "21f2001453@student.onlinedegree.iitm.ac.in",
+        }]
     },
     {
         title: "Paradox Unwind",
@@ -201,12 +197,10 @@ const cultural = [
         desc: "Come on join the exciting Voyage of music melodies and madness from different genres, cultures and languages all united in one stage ready to take you on an amazing journey of euphoria, love, excitement, refreshment and what not..! <br> So buckle up and be a part of this mesmerizing journey.",
         date: "20th May, 08:00pm-10:00pm",
         venue: "20/05/2022 - Students Activity Center (SAC)",
-        contact: [
-            {
-                name: "Will be updated Updated Soon, contact paradox support",
-                email: "Updated Soon, contact paradox support",
-            }
-        ]
+        contact: [{
+            name: "Will be updated Updated Soon, contact paradox support",
+            email: "Updated Soon, contact paradox support",
+        }]
     },
     {
         title: "Qutopia",
@@ -215,8 +209,7 @@ const cultural = [
         desc: "Qutopia is the inaugural quiz module under IITM Paradox'22.<br>The flagship General Quiz shall be held in 2 parts:<br><br>1. Prelims on 20th May: Participants shall team up to compete against other teams, to qualify for a spot amongst the Top 8 teams playing in the finals on 21st May. Rules Document:  https://tinyurl.com/qutopia-rules <br><br> 2. Finals: Instructions shall be conveyed to the top 8 qualified teams. It will be conducted on 21st May.",
         date: "Round 1 - 20th May, 01:30pm-03:30pm - Preliminary Round <br><br> Round 2 - 21st May, 03:30pm-05:30pm - FINALE",
         venue: "Preliminary Round - 20/05/2022<br><br>FINALE - 21/05/2022 - Student Activity Center (SAC)",
-        contact: [
-            {
+        contact: [{
                 name: "Lakshya Raj",
                 email: "21f1003767@student.onlinedegree.iitm.ac.in",
             },
@@ -233,30 +226,25 @@ const cultural = [
         desc: "Tired of hustling around with the events? Why not take a break for a while. Just relax yourself with calm and soothing music. The magnificent ambience might help in creating long-lasting memories. Get to know the crowd around you with a chilling and truly splendid atmosphere. The sight of hanging lights would be enchanting. Unique and spontaneous night with a bunch of people and a bit of chit-chat is gonna be memorable.",
         date: "20th May, 10:00pm-02:00am",
         venue: "20/05/2022 - Students Activity Center (SAC)",
-        contact: [
-            {
-                name: "Mihir Gupta",
-                email: "21f1001154@student.onlinedegree.iitm.ac.in",
-            }
-        ]
+        contact: [{
+            name: "Mihir Gupta",
+            email: "21f1001154@student.onlinedegree.iitm.ac.in",
+        }]
     }
 
 ]
 
-const professional = [
-    {
+const professional = [{
         title: "The Outliers",
         url: '',
         imageID: 'theoutliers.jpg',
         desc: "Team Outliers - Entrepreneurship Workshop is coming out with an unique lineup of events in the IITM Paradox'22, the Annual Fest of the IITM BSc Degree Community. The whole Entrepreneurship Workshop will consist of two exciting episodes : The Conference & The Start-Up Showcase.<br><br>Focusing on the theme of Entrepreneurial Skills & Mindset, we are inviting registrations for The Conference. The whole episode consists of talk and interaction with Prof. G Venkatesh (Professor of Practice at IITM, Director of Sasken Technologies Limited, CEO of Mylspot Education Services), one-on-one Q&A session with the audience and much more.<br><br>Participation E-certificates will be provided from the Entrepreneurship Workshop team.",
         date: "20th May, 2022, 2:00 pm to 4:00 pm",
         venue: "SAC",
-        contact: [
-            {
-                name: "Sankalp Arora",
-                email: "21f1004054@student.onlinedegree.iitm.ac.in",
-            }
-        ]
+        contact: [{
+            name: "Sankalp Arora",
+            email: "21f1004054@student.onlinedegree.iitm.ac.in",
+        }]
     },
     {
         title: "Hack-o-Pitch",
@@ -265,8 +253,7 @@ const professional = [
         desc: "Check out the hack-o-pitch Website for more details: <a href='https://www.hackopitch.tech' target='_blank'>Click Here</a>",
         date: "02:00 pm - 06:00 pm",
         venue: "CLT/ICSR Main Auditorium",
-        contact: [
-            {
+        contact: [{
                 name: "Ankit Singh",
                 email: "21f1001955@student.onlinedegree.iitm.ac.in",
             },
@@ -283,8 +270,7 @@ const professional = [
         desc: "A compilation of the best projects from Mordern Application Development - I, Mordern Application Development - II and Business Data Management. ",
         date: "12:00 pm - 01:00 pm",
         venue: "Student Activity Center (SAC)",
-        contact: [
-        ]
+        contact: []
     }
 ]
 
